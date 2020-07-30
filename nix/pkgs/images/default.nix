d50294f4333af9c4cfbfbc1f416ef5e4b9b4149d
Jan Kryl <jan.kryl@mayadata.io>
2020-07-30 15:40:33 +0000

    Proper mayastor debug package, right package versions, cleanup images.
    There are two flavours of mayastor nix package: mayastor and
    mayastor-dev. And they don't get accidentaly confused by the build
    system as was happening before with "release" parameter.
    
    Overhaul of nix code for creating docker images. No code duplication
    and intuitive target names. All image targets end with "-image" suffix.
    The flavour of the package that the image is based on (if any) preceeds
    the suffix (i.e. dev or adhoc).
    
    We are able to deduce the accurate version number of mayastor package
    in effortless way. It uses a git command to lookup a tag for the commit
    and if tag does not exist, it uses short commit hash. So it never gets
    stale.

--- nix/pkgs/images/default.nix
+++ nix/pkgs/images/default.nix
@@ -2,65 +2,23 @@
 , busybox
 , dockerTools
 , e2fsprogs
+, git
 , lib
-, libaio
-, libiscsi
-, libspdk
-, libudev
-, liburing
-, openssl
-, utillinux
 , writeScriptBin
 , xfsprogs
 , mayastor
-, sources
+, mayastor-dev
+, mayastor-adhoc
 }:
-
-rec {
-
+let
+  version_drv = import ../../lib/version.nix { inherit lib stdenv git; };
+  version = builtins.readFile "${version_drv}";
   env = stdenv.lib.makeBinPath [ busybox xfsprogs e2fsprogs ];
 
-  # image that does not do a build
-  mayastor-adhoc = stdenv.mkDerivation {
-    name = "mayastor-dev";
-    version = "1.0";
-    src = [
-      ../../../target/debug/mayastor
-      ../../../target/debug/mayastor-csi
-      ../../../target/debug/mayastor-client
-      ../../../target/debug/jsonrpc
-    ];
-
-    buildInputs = [
-      libaio
-      libiscsi.lib
-      libspdk
-      liburing
-      libudev
-      openssl
-      xfsprogs
-      e2fsprogs
-    ];
-
-    unpackPhase = ''
-      for srcFile in $src; do
-         cp $srcFile $(stripHash $srcFile)
-      done
-    '';
-    dontBuild = true;
-    dontConfigure = true;
-    installPhase = ''
-      mkdir -p $out/bin
-      install * $out/bin
-    '';
-  };
-
-
-  quick = dockerTools.buildImage {
-    name = "mayadata/mayastor";
-    tag = "adhoc";
+  # common props for all mayastor images
+  mayastor_image_props = {
+    tag = version;
     created = "now";
-    contents = [ busybox mayastor-adhoc ];
     config = {
       Env = [ "PATH=${env}" ];
       ExposedPorts = { "10124/tcp" = { }; };
@@ -69,66 +27,43 @@ rec {
     # This directory is for mayastor jsonrpc socket file
     extraCommands = "mkdir -p var/tmp";
   };
-
-  mayastor-image-release = dockerTools.buildImage {
-    name = "mayadata/mayastor";
-    tag = sources.mayastor.branch;
+  mayastor_csi_image_props = {
+    tag = version;
     created = "now";
+    config = {
+      Entrypoint = [ "/bin/mayastor-csi" ];
+      Env = [ "PATH=${env}" ];
+    };
+  };
+in
+rec {
+  mayastor-image = dockerTools.buildImage (mayastor_image_props // {
+    name = "mayadata/mayastor";
     contents = [ busybox mayastor ];
-    config = {
-      Env = [ "PATH=${env}" ];
-      ExposedPorts = { "10124/tcp" = { }; };
-      Entrypoint = [ "/bin/mayastor" ];
-    };
-    # This directory is for mayastor jsonrpc socket file
-    extraCommands = "mkdir -p var/tmp";
-  };
+  });
+
+  mayastor-dev-image = dockerTools.buildImage (mayastor_image_props // {
+    name = "mayadata/mayastor-dev";
+    contents = [ busybox mayastor-dev ];
+  });
+
+  mayastor-adhoc-image = dockerTools.buildImage (mayastor_image_props // {
+    name = "mayadata/mayastor-adhoc";
+    contents = [ busybox mayastor-adhoc ];
+  });
 
   mayastorIscsiadm = writeScriptBin "mayastor-iscsiadm" ''
     #!${stdenv.shell}
     chroot /host /usr/bin/env -i PATH="/sbin:/bin:/usr/bin" iscsiadm "$@"
   '';
 
-  csi-release = dockerTools.buildLayeredImage {
+  mayastor-csi-image = dockerTools.buildLayeredImage (mayastor_csi_image_props // {
     name = "mayadata/mayastor-csi";
-    tag = sources.mayastor.branch;
-    created = "now";
     contents = [ busybox mayastor mayastorIscsiadm ];
-    config = {
-      Entrypoint = [ "/bin/mayastor-csi" ];
-      Env = [ "PATH=${env}" ];
-    };
-  };
+  });
 
-
-  # images during CI
-
-  mayastor-develop = mayastor.override { release = false; };
-
-  mayastor-image-develop = dockerTools.buildImage {
-    name = "mayadata/mayastor";
-    tag = "develop";
-    created = "now";
-    contents = [ busybox mayastor-develop ];
-    config = {
-      Env = [ "PATH=${env}" ];
-      ExposedPorts = { "10124/tcp" = { }; };
-      Entrypoint = [ "/bin/mayastor" ];
-    };
-    # This directory is for mayastor jsonrpc socket file
-    extraCommands = "mkdir -p var/tmp";
-  };
-
-  mayastor-csi-develop = dockerTools.buildImage {
-    name = "mayadata/mayastor-csi";
-    tag = "develop";
-    created = "now";
-    contents = [ busybox mayastor-develop mayastorIscsiadm ];
-    config = {
-      Env = [ "PATH=${env}" ];
-      ExposedPorts = { "10124/tcp" = { }; };
-      Entrypoint = [ "/bin/mayastor-csi" ];
-    };
-  };
-
+  mayastor-csi-dev-image = dockerTools.buildImage (mayastor_csi_image_props // {
+    name = "mayadata/mayastor-csi-dev";
+    contents = [ busybox mayastor-dev mayastorIscsiadm ];
+  });
 }
